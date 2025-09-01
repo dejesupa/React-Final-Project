d17d8894b4f59829a086e25be609488cca61a5e8
Trisha <trisha.a.dejesus@gmail.com>
2025-09-01 18:39:32 -0400

    filtering2

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,40 +1,41 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-let currentMovies = [];
-
 const Search = () => {
-    const [option, newOption] = useState();
+  const [option, setOption] = useState([]);
 
-    function filterMovies(filter) {
-        console.log(filter)
+  function filterMovies(filter) {
+    console.log (filter)
 
-        if (filter === 'Newest') {
-            newOption(option.slice().sort((a, b) => (b.Year) - (a.Year)))
-        }
-
-       
+    if (filter === "Newest") {
+      setOption(option.slice().sort((a, b) => b.Year - a.Year));
     }
+    if (filter === "Oldest") {
+      setOption(option.slice().sort((a, b) => a.Year - b.Year));
+    }
+ 
+  }
 
-//  function sortChange(event) {
-//         const sortOption = event.target.value
+  //  function sortChange(event) {
+  //         const sortOption = event.target.value
 
-//         let sortedMovies = [...currentMovies]
+  //         let sortedMovies = [...currentMovies]
 
-//         if (sortOption === "Newest") {
-//             sortedMovies.sort((a, b) => (b.Year) - (a.Year));
-//         }
-//         else if (sortOption === "Oldest") {
-//             sortedMovies.sort((a, b) => (a.Year) - (b.Year));
-//         }
+  //         if (sortOption === "Newest") {
+  //             sortedMovies.sort((a, b) => (b.Year) - (a.Year));
+  //         }
+  //         else if (sortOption === "Oldest") {
+  //             sortedMovies.sort((a, b) => (a.Year) - (b.Year));
+  //         }
 
-//         displayMovies(sortedMovies);
-
+  //         displayMovies(sortedMovies);
 
   const [movies, setMovies] = useState([]);
 
   async function getMovies() {
-    const { data } = await axios.get("https://www.omdbapi.com/?apikey=c24b97b7&s=fast");
+    const { data } = await axios.get(
+      "https://www.omdbapi.com/?apikey=c24b97b7&s=fast"
+    );
     setMovies(data.Search);
   }
 
@@ -44,7 +45,6 @@ const Search = () => {
 
   return (
     <section id="search">
-       
       <div className="container search__container">
         <div className="row">
           <div className="search__header">
@@ -54,7 +54,11 @@ const Search = () => {
                 <span className="search__results"></span>
               </h1>
             </div>
-            <select id="filter" defaultValue="DEFAULT" onChange={(event) => filterMovies(event.target.value)}>
+            <select
+              id="filter"
+              defaultValue="DEFAULT"
+              onChange={(event) => filterMovies(event.target.value)}
+            >
               <option value="" disabled>
                 Sort
               </option>
@@ -65,21 +69,19 @@ const Search = () => {
               <i className="fas fa-spinner books__loading--spinner"></i>
             </div>
           </div>
-     
-     
         </div>
       </div>
-       <div className="movie__list"> 
-          {movies.map((movie) => (
-        <div className="movie" key={movie.Title}>
-                <figure className="movie__img--wrapper">
-                  <img className="movie__img" src={movie.Poster} alt="" />
-                </figure>
-                <div className="movie__title">{movie.Title}</div>
-                <div className="movie__year">{movie.Year}</div>
-              </div>
-      )) }
-        </div>
+      <div className="movie__list">
+        {movies.map((movie) => (
+          <div className="movie" key={movie.Title}>
+            <figure className="movie__img--wrapper">
+              <img className="movie__img" src={movie.Poster} alt="" />
+            </figure>
+            <div className="movie__title">{movie.Title}</div>
+            <div className="movie__year">{movie.Year}</div>
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
