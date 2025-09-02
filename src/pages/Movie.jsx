a96e94ee3ca46ab61e94bf18f6e0677ceeb81ed2
Trisha <trisha.a.dejesus@gmail.com>
2025-09-02 18:05:00 -0400

    trying to map

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,27 +1,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 
 const Movie = () => {
+  const { imdbID } = useParams();
+  const [movies, setMovies] = useState({});
 
-const { imdbID } = useParams;
-const [movies, setMovies] = useState([]);
-
-
-async function getMovies() {
+  async function getMovies() {
     const { data } = await axios.get(
-      `https://www.omdbapi.com/?apikey=c24b97b7&s=${imdbID}`
+      `https://www.omdbapi.com/?apikey=c24b97b7&i=${imdbID}`
     );
-    setMovies(data.Search);
-
-    //   const movie = movies.find(movie => +movies.id === +id)
-    //   console.log(getMovies)
+    setMovies(data);
   }
 
-
- 
-
+  useEffect(() => {
+    if (imdbID) {
+      getMovies(imdbID);
+    }
+  }, [imdbID]);
 
   return (
     <div id="movie__body">
@@ -36,32 +33,37 @@ async function getMovies() {
                 <h2 className="movie__selected--title">Movies</h2>
               </Link>
             </div>
-            <div className="movie__selected">
-              <figure className="movie__selected--figure">
-                <img
-                  src={"https://m.media-amazon.com/images/M/MV5BZGRiMDE1NTMtMThmZS00YjE4LWI1ODQtNjRkZGZlOTg2MGE1XkEyXkFqcGc@._V1_SX300.jpg"}
-                  alt=""
-                  className="book__selected--img"
-                />
-              </figure>
-              <div className="book__selected--description">
-                <h2 className="book__selected--title">
-              Movie Title
-                </h2>
-                <div className="book__selected--year">
-                    2003
-                    </div>
-                <div className="movie__summary--title">
-                    Summary
+            <div className="movie__selected--wrapper">
+              {movies.map((movie) => (
+                <div className="movie__selected" key={movie.Title}>
+                  <figure className="movie__selected--figure">
+                    <img
+                      src={
+                        movie.Poster
+                      }
+                      alt=""
+                      className="book__selected--img"
+                    />
+                  </figure>
+                  <div className="book__selected--description">
+                    <h2 className="book__selected--title">{movie.Title}</h2>
+                    <div className="book__selected--year">{movie.Year}</div>
+                    <div className="movie__summary--title">Summary</div>
+                    <p className="book__summary--para">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Officiis eos id tempore veniam illum. Illum iste nostrum
+                      aperiam, voluptate sed quasi repellat, incidunt quis, sit
+                      nesciunt aut deserunt nulla dolor.
+                    </p>
+                    <p className="book__summary--para">
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Officiis eos id tempore veniam illum. Illum iste nostrum
+                      aperiam, voluptate sed quasi repellat, incidunt quis, sit
+                      nesciunt aut deserunt nulla dolor.
+                    </p>
+                  </div>
                 </div>
-                <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis eos id tempore veniam illum. Illum iste nostrum aperiam, voluptate sed quasi repellat, incidunt quis, sit nesciunt aut deserunt nulla dolor.
-                </p>
-                <p className="book__summary--para">
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis eos id tempore veniam illum. Illum iste nostrum aperiam, voluptate sed quasi repellat, incidunt quis, sit nesciunt aut deserunt nulla dolor.
-                </p>
-             
-              </div>
+              ))}
             </div>
           </div>
         </div>
